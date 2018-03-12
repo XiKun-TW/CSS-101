66eb5d8e4e114239108a8d46693a280fe0820ad1
Gougougogogo <394442302@qq.com>
2018-03-12 23:14:40 +0800

    add css model classes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-import React from "react";
-import { render } from "react-dom";
-git;
-
 import "./scss/index.scss";
-
-const App = props => <h1>Hello world</h1>;
-App.propTypes = {};
-
-window.onload = () => {
-  render(<App />, document.querySelector("#app"));
-};
+import "./scss/boxModel.scss";
